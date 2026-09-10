d6d13e92b637273e9ad105c15147a6f701fd40b7
Esellio <pirko.vichmann@voco.ee>
2026-09-10 14:57:06 +0300

    Added Flowbite to be able to use a UI library

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,8 @@
 <html>
+    <head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    
     <body>
         <h1>
             Please enter your name and pick the Sectors you are currently involved in.
@@ -8,5 +12,7 @@
         <br>
 
         <livewire:sectors-form />
+
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     </body>
 </html>
