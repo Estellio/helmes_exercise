5780e5256004ed01b620f4e01cbd8343254f6cb3
Esellio <pirko.vichmann@voco.ee>
2026-09-12 17:37:12 +0300

    Changed main layout and extracted validation error into a component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,25 @@
-<html>
+<html class="group/root dark">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Helmes exercise') }}</title>
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     
-    <body>
-        <h1>
-            Please enter your name and pick the Sectors you are currently involved in.
-        </h1>
-        
-        <br>
-        <br>
+    <body class="flex flex-col items-center p-10 bg-gray-900 text-gray-300">
+        <div class="flex flex-col w-full gap-y-8 bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/50 min-w-[360px] max-w-[900px]">
+            <h1 class="text-white font-medium text-lg">
+                Please enter your name and pick the Sectors you are currently involved in.
+            </h1>
 
-        <livewire:sectors-form />
+            <livewire:sectors-form />
+        </div>
+
+        <div>
+            {{ session()->getId() }}
+        </div>
 
         <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     </body>
